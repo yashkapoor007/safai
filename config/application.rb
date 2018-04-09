1538be35d76d0ce58c20d74018446128dce5a265
yashkapoor007 <ykapoor_be15@thapar.edu>
2018-04-09 12:20:25 +0530

    heroku config

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Safai
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.version = '1.0'
+config.assets.initialize_on_precompile = false
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
